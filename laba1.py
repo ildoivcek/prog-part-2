42352143243c8e14f21d31c754d5fd97cd36d89c
ildoivcek <ann270108@gmail.com>
2026-02-24 08:51:50 +0200

    Delete unit tests from laba1.py
    Removed unit tests for the longest_peak function.

--- laba1.py
+++ laba1.py
@@ -21,31 +21,5 @@ def longest_peak(array):
 
     return max_length
 
-class TestLongestPeak(unittest.TestCase):
-
-    def test_example(self):
-        self.assertEqual(longest_peak([1, 3, 5, 4, 2, 8, 3, 7]), 5)
-
-    def test_sorted_zrostae(self):
-        self.assertEqual(longest_peak([1, 2, 3, 4, 5]), 0)
-
-    def test_sorted_spadae(self):
-        self.assertEqual(longest_peak([5, 4, 3, 2, 1,]), 0)
-
-    def test_two_elements(self):
-        self.assertEqual(longest_peak([1, 2]), 0)
-
-    def test_no_peaks(self):
-        self.assertEqual(longest_peak([2, 2, 2]), 0)
-        self.assertEqual(longest_peak([-1, -5, -1]), 0)
-
-    def test_three_peaks(self):
-        self.assertEqual(longest_peak([1, 3, 2, 1, 5, 4, 1, 4, 8, 6, 2]), 5)
-
-
-if __name__ == '__main__':
-    unittest.main()
-           
-
  
            
